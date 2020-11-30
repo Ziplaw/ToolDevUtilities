a563f9dd0851e1e0662fc0983b79072871c2ad43
Ziplaw <afrgplus@gmail.com>
2020-11-30 18:10:04 +0100

    Initial Commit

---
+++ Scripts/Editor/ScriptTemplateCreator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+public class CreateNewScriptClassFromCustomTemplate
+{
+    private const string pathToYourScriptTemplate = "Assets/ToolCreationUtilities/Templates/NewEditorScript.cs.txt";
+ 
+    [MenuItem("CONTEXT/MonoBehaviour/Create Editor Script")]
+    public static void CreateScriptFromTemplate(MenuCommand cmd)
+    {
+        if (AssetDatabase.IsValidFolder("Assets/Editor"))
+        {
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(pathToYourScriptTemplate,
+                $"Assets/Editor/{cmd.context.GetType().ToString()}.cs");
+        }
+        else
+        {
+            AssetDatabase.CreateFolder("Assets", "Editor");
+            ProjectWindowUtil.CreateScriptAssetFromTemplateFile(pathToYourScriptTemplate,
+                $"Assets/Editor/{cmd.context.GetType().ToString()}.cs");
+        }
+    }
+}
